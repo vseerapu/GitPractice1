1fc4e056b2ed44d40e33f59549b92065c61fdfd7
vseerapu <vseerapu@gmail.com>
2022-10-12 16:35:52 -0600

    Git Practice 1

---
+++ src/com/myproject/TestCase2.java
@@ -0,0 +1,12 @@
+package com.myproject;
+
+public class TestCase2 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Every class file should be created within a package");
+
+	}
+
+}
+
